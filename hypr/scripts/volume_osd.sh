7b672f468d8c8fae6efdc416d9ca305eb42dc61e
Sayantan-Bhowmick <sayantanbhowmick27523@gmail.com>
2023-10-18 02:27:20 +0530

    minor changes

--- hypr/scripts/volume_osd.sh
+++ hypr/scripts/volume_osd.sh
@@ -7,10 +7,10 @@ multiplier=100
 current_volume=$(wpctl get-volume @DEFAULT_SINK@ | awk '{print $2}')
 
 # Calculate the current volume percentage by multiplying with the multiplier
-current_volume_percentage=$(echo "scale=2; $current_volume * $multiplier" | bc)
+current_volume_percentage=$(echo "$current_volume * $multiplier" | bc)
 
 # Round the volume percentage to the nearest integer
-display_volume=$(printf "%2.0f" $current_volume_percentage)
+display_volume=$(printf "%.0f" $current_volume_percentage)
 
 # Restart the Dunst notification daemon to prevent any potential issues
 killall dunst
