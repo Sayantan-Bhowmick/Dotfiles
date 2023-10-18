5d1810e54cb577d4bd567606f7ddc4027114d39e
Sayantan-Bhowmick <sayantanbhowmick27523@gmail.com>
2023-10-18 22:59:22 +0530

    minor changes

--- hypr/scripts/set_wallpaper.sh
+++ hypr/scripts/set_wallpaper.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
-# Directory to select a file from
-directory="$HOME/Pictures/Wallpapers/"
+Directory="$HOME/Pictures/Wallpapers/"
 
-# Get a list of files in the directory
-files=("$directory"/*)
+random_file=$(find "$Directory" -type f | shuf --random-source="/dev/random" -n 1)
 
-# Generate a random index
-random_index=$((RANDOM % ${#files[@]}))
-
-# Get the randomly selected file
-random_file="${files[$random_index]}"
-
-# Get the absolute path of the random file
-absolute_path=$(realpath "$random_file")
-
-export SWWW_TRANSITION=grow
+export SWWW_TRANSITION=any
+export SWWW_TRANSITION_STEP=225
+export SWWW_TRANSITION_DURATION=2.5
 export SWWW_TRANSITION_FPS=60
-export SWWW_TRANSITION_STEP=150
-export SWWW_TRANSITION_POS=center
 
-swww img --outputs eDP-1 $absolute_path
+swww img --resize fit --outputs eDP-1 "$random_file"
