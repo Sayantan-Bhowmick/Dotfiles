fe8c287dd4b5a43e4f308dd7bd9487edde8216ce
Sayantan-Bhowmick <sayantanbhowmick27523@gmail.com>
2023-10-18 13:42:23 +0530

    minor changes

--- hypr/scripts/brightness_osd.sh
+++ hypr/scripts/brightness_osd.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-# Get the current brightness using brightnessctl
-current_brightness=$(brightnessctl get)
+# Current brightness percentage
+current_brightness_percentage=$(brightnessctl -m | cut -d, -f4)
 
-# Calculate the current brightness percentage
-current_brightness_percentage=$((current_brightness / 15))
-
-# Restart the Dunst notification daemon to prevent any potential issues
+# Restart the dunst notification daemon to prevent any potential issues
 killall dunst
 dunst &
 
 # Create a notification using dunstify with brightness information
-dunstify --appname "SYSTEM" --urgency "LOW" --hints int:value:$current_brightness_percentage --timeout "1500" "Brightness: $current_brightness_percentage%"
+dunstify --appname "SYSTEM" --urgency "LOW" --hints int:value:$current_brightness_percentage --timeout "1500" "Brightness: $current_brightness_percentage"
