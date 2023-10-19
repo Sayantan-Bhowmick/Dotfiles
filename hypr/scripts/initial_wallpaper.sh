c496643f6b8d87ea331ddf6d2468c4e997ba78c5
Sayantan-Bhowmick <sayantanbhowmick27523@gmail.com>
2023-10-19 12:39:36 +0530

    minor changes

--- hypr/scripts/initial_wallpaper.sh
+++ hypr/scripts/initial_wallpaper.sh
@@ -7,5 +7,5 @@ export SWWW_TRANSITION_DURATION=2.5        # Transition duration: 2.5 seconds
 export SWWW_TRANSITION_FPS=60              # Frames per second (FPS) for the transition: 60
 
 # Change the wallpaper using the 'swww img' command
-sleep 1
+sleep 2
 swww img --resize fit --outputs eDP-1 $HOME/Pictures/Wallpapers/wallhaven-ymz61d_3840x2160.png
