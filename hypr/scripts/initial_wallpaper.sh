1eb6364649131820ed81e418cf1d908c26bcabd3
Sayantan-Bhowmick <sayantanbhowmick27523@gmail.com>
2023-10-27 21:49:01 +0530

    minor changes

--- hypr/scripts/initial_wallpaper.sh
+++ hypr/scripts/initial_wallpaper.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Configure transition settings for wallpaper changes
-export SWWW_TRANSITION=any                 # Transition style: "any"
-export SWWW_TRANSITION_STEP=225            # Transition step: 225 (controls speed or progress)
-export SWWW_TRANSITION_DURATION=2.5        # Transition duration: 2.5 seconds
-export SWWW_TRANSITION_FPS=60              # Frames per second (FPS) for the transition: 60
+# Set the directory path to your wallpaper images
+Directory="$HOME/.config/hypr/wallpapers/"
+
+# Find and select a random image file from the specified directory
+random_file=$(find "$Directory" -type f | shuf --random-source="/dev/random" -n 1)
 
 # Change the wallpaper using the 'swww img' command
 sleep 1
-swww img --resize fit --outputs eDP-1 "$HOME/.config/hypr/wallpapers/wallhaven-j53zxy_3840x2160.png"
+swww img --resize fit --outputs eDP-1 "$random_file"
